838afcdc68e2a6023e78046b010363e3a5de9764
Andrey Myagkov <andreyoren@gmail.com>
2023-02-20 20:04:02 +0500

    Mnager Component

--- src/components/Search/Manager.vue
+++ src/components/Search/Manager.vue
@@ -1,3 +1,58 @@
 <template>
-    1
+    <div class="ks-block ks-manager">
+        <div class="ks-manager__item">
+            <div class="ks-row">
+                <div class="ks-manager__figure ks-col-2">
+                    <img src="https://images.generated.photos/tg0NGLDdDZ_rUDbLWr0EU_3ZIv1gpV5sjCc9xUa-sQs/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy90cmFu/c3BhcmVudF92My92/M18wNTE1MjIzLnBu/Zw.png" alt="" class="ks-manager__img" width="130" height="130">
+                    <div class="ks-manager__name ks-p">Alexa Alexxxxxxxa</div>
+                </div>
+                <div class="ks-manager__info ks-col-10">
+                    <div class="ks-manager__header ks-h3">Heading Heading Heading Heading Heading Heading Heading</div>
+                    <div class="ks-manager__phone">02232 307 98 0</div>
+                    <div class="ks-manager__worktime ks-p">Mo-So 09:00 - 22:00</div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+
+<style scoped>
+.ks-manager {
+    padding: 0 40px;
+    margin-bottom: 30px;
+}
+
+.ks-manager__item {
+    padding: 30px;
+}
+
+.ks-manager__figure {
+
+}
+
+.ks-manager__img {
+    display: block;
+    aspect-ratio: 1;
+    object-fit: cover;
+    margin-bottom: 5px;
+    border: solid 1px var(--ks-control-border-color);
+    border-radius: var(--ks-border-radius);
+}
+
+.ks-manager__name {
+
+}
+
+.ks-manager__info {}
+
+.ks-manager__header {
+    margin-bottom: 43px;
+}
+
+.ks-manager__phone {
+    font-size: 30px;
+    line-height: 38px;
+}
+
+.ks-manager__worktime {}
+</style>
